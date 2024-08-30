e4530f9cfffb1d3822d1bd1c6cacded48d2bc61b
Sunil Bombe <sunil.bombe1998@gmail.com>
2024-08-30 21:44:57 +0530

    Added Request and Response Files for login

---
+++ src/main/java/com/ecom/e_commerce_user_service/dto/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.ecom.e_commerce_user_service.dto;
+
+public class LoginRequest {
+
+    private String username;
+
+    private String password;
+
+
+}
---
+++ src/main/java/com/ecom/e_commerce_user_service/service/impl/LoginService.java
@@ -0,0 +1,5 @@
+package com.ecom.e_commerce_user_service.service.impl;
+
+public interface LoginService {
+
+}
