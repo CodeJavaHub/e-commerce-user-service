98006a0f4cacd1974a32901c406d1a6168e8d07a
Sunil Bombe <sunil.bombe1998@gmail.com>
2024-09-12 23:45:34 +0530

    Added user signup form template URL for code reuse

---
+++ src/main/java/com/ecom/userservice/dto/FormDataDto.java
@@ -0,0 +1,15 @@
+package com.ecom.userservice.dto;
+
+import java.util.Map;
+
+public class FormDataDto {
+  private Map<String, Object> data;
+
+  public Map<String, Object> getData() {
+    return data;
+  }
+
+  public void setData(Map<String, Object> data) {
+    this.data = data;
+  }
+}
